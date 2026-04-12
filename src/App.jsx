0ead1fe0dea981f1e849979fc931c0ef8f2bc450
Osama Farag <ofarag115@gmail.com>
2026-04-12 15:26:11 -0400

    Added routes for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast"
+import Home from "./pages/Home.jsx";
+import Income from "./pages/Income.jsx";
+import Expense from "./pages/Expense.jsx";
+import Category from "./pages/Category.jsx";
+import Filter from "./pages/Filter.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import { Navigate } from "react-router-dom";
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">Money Manager</div>
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" replace />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/signup" element={<Signup />} /> 
+        </Routes>
+      </BrowserRouter> 
+    </>
   )
 }
 
