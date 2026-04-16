58fa5dc7f5b318660214f0de08f51677b031458c
Osama Farag <ofarag115@gmail.com>
2026-04-16 09:30:39 -0400

    add isLoading state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error , setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
 
     const navigate = useNavigate();
